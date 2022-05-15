2022ba992106a17f6d22892d07a53103ed8d971d
burak48 <buraksaglam3@gmail.com>
2022-05-15 14:00:53 +0300

    more numbers test added

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -7,8 +7,25 @@ it('should transform a string number to a number of type number', () => {
     expect(result).toBeTypeOf('number');
 });
 
+it('should transform a string number to a number of type number - v2', () => {
+    const input = '1';
+    const result = transformToNumber(input);
+    expect(result).toBe(+input);
+});
+
 it('should yield NaN for non-transformable values', () => {
     const input = 'invalid';
     const result = transformToNumber(input);
     expect(result).toBeNaN();
+});
+
+it('should yield NaN for non-transformable values - v2', () => {
+    const input = 'invalid';
+    const input2 = {};
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
 });
