625863ef673e0ec59ebbecfee0ecf38a00a7063a
burak48 <buraksaglam3@gmail.com>
2022-05-15 13:45:57 +0300

    more math test added

--- src/math.test.js
+++ src/math.test.js
@@ -38,3 +38,15 @@ it('should throw an error if no value is passed into the function', () => {
     };
     expect(resultFunc).toThrow();
 });
+
+it('should throw an error if provided with multiple arguments instead of an array', () => {
+    const num1 = 1;
+    const num2 = 2;
+    
+    const resultFn = () => { 
+        add(num1, num2);
+    }
+
+    // expect(resultFn).not.toThrow();
+    expect(resultFn).toThrow(/is not iterable/);
+});
