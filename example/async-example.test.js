e833e0dc978bdbd49f3db0b62e4903e352eb34b9
burak48 <buraksaglam3@gmail.com>
2022-05-19 21:07:25 +0300

    asynchronous code and test added

---
+++ example/async-example.test.js
@@ -0,0 +1,12 @@
+import { it, expect, describe } from 'vitest';
+import { generateToken } from './async-example';
+
+describe('generateToken()', () => {
+    it('should generate a token value', () => {
+        const testUserEmail = 'test@test.com';
+        generateToken(testUserEmail, (err, token) => {
+            // expect(token).toBeDefined();
+            expect(token).toBe(2);
+        })
+    });
+});
