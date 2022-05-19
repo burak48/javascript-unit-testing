e88fd78e885daf492f42d3dc266077fac453d931
burak48 <buraksaglam3@gmail.com>
2022-05-19 21:20:04 +0300

    async test improved

--- example/async-example.test.js
+++ example/async-example.test.js
@@ -2,11 +2,16 @@ import { it, expect, describe } from 'vitest';
 import { generateToken } from './async-example';
 
 describe('generateToken()', () => {
-    it('should generate a token value', () => {
+    it('should generate a token value', (done) => {
         const testUserEmail = 'test@test.com';
         generateToken(testUserEmail, (err, token) => {
-            // expect(token).toBeDefined();
-            expect(token).toBe(2);
+            try { 
+                expect(token).toBeDefined();
+                // expect(token).toBe(2);
+                done();
+            } catch (err) {
+                 done(err);
+            }
         })
     });
 });
