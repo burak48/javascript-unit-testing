9efc91f2dd1aea22f2fe131e185332fa3f4f684a
burak48 <buraksaglam3@gmail.com>
2022-05-22 22:57:19 +0300

    side effects example added

---
+++ example/side_effects/src/util/io.test.js
@@ -0,0 +1,9 @@
+import { it, expect, describe } from 'vitest';
+import writeData from './io';
+
+
+it('should execute the writeFile method', () => {
+    const testData = 'Test';
+    const testFileName = 'test.txt';
+    return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+});
