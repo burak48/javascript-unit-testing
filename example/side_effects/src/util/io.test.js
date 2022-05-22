71ba184151256e6195996fcebcc5b9176a4647ac
burak48 <buraksaglam3@gmail.com>
2022-05-22 23:46:58 +0300

    mock function improved

--- example/side_effects/src/util/io.test.js
+++ example/side_effects/src/util/io.test.js
@@ -1,16 +1,20 @@
 import { it, expect, describe, vi } from 'vitest';
+import { promises as fs } from 'fs';
+
 import writeData from './io';
 
 vi.mock('fs');
 
-it('should execute the writeFile method', () => {
-    const testData = 'Test';
-    const testFileName = 'test.txt';
-    return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
-});
+// it('should execute the writeFile method', () => {
+//     const testData = 'Test';
+//     const testFileName = 'test.txt';
+//     return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+// });
 
 it('should execute the writeFile method - v2', () => {
     const testData = 'Test';
     const testFileName = 'test.txt';
-    // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+    
+    writeData(testData, testFileName)
+    expect(fs.writeFile).toBeCalled();
 });
