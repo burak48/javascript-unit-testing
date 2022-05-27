3d3608e05244a5bf6946af93858d373fd71ef298
burak48 <buraksaglam3@gmail.com>
2022-05-28 01:05:42 +0300

    test command changed due to dom side effects && dom test added

---
+++ example/mocking/util/dom.test.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+import { it, expect, vi } from "vitest";
+import { Window } from 'happy-dom';
+import { showError } from "./dom";
+
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+
+document.write(htmlDocContent);
+vi.stubGlobal('document', document);
+
+it('first test', () => {
+    showError('test');
+});
