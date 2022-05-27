0ee9ace7fe29870e4dbc2b88884d033b4021498a
burak48 <buraksaglam3@gmail.com>
2022-05-28 00:20:05 +0300

    http test improved

--- example/mocking/util/http.test.js
+++ example/mocking/util/http.test.js
@@ -4,6 +4,9 @@ import { sendDataRequest } from "./http";
 const testResponseData = { testKey: 'testData' };
 
 const testFetch = vi.fn((url, options) => {
+    if (typeof options.body !== 'string') {
+        return reject('Not a string');
+    }
     return new Promise((resolve, reject) => {
         const testResponse = {
             ok: true,
@@ -22,4 +25,16 @@ vi.stubGlobal('fetch', testFetch);
 it('should return any available response data', () => {
     const testData = { key: 'test' };
     return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it('should convert the provided data to JSON before sending the request', async () => {
+    const testData = { key: 'test' };
+    let errorMessage;
+
+    try {
+        await sendDataRequest(testData);
+    } catch (error) {
+        errorMessage = error;
+    }
+    expect(errorMessage).not.toBe('Not a string');
 });
