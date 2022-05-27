6218b3df31d62be2f45a15584e868dbd0ddcf63a
burak48 <buraksaglam3@gmail.com>
2022-05-28 00:43:53 +0300

    posts test improved

--- example/mocking/posts/posts.test.js
+++ example/mocking/posts/posts.test.js
@@ -1,19 +1,23 @@
+import { beforeEach } from "vitest";
 import { describe, it, expect } from "vitest";
 import { extractPostData } from "./posts";
 
+const testTitle = 'Test title';
+const testContent = 'Test content';
+let testFormData;
 
 describe('extractPostData()', () => {
-    it('should extract title and content from the provided form data', () => {
-        const testTitle = 'Test title';
-        const testContent = 'Test content';
-        const testFormData = {
+    beforeEach(() => {
+        testFormData = {
             title: testTitle,
             content: testContent,
             get(identifier) {
                 return this[identifier];
             },
         };
+    });
 
+    it('should extract title and content from the provided form data', () => {
         const data = extractPostData(testFormData);
 
         expect(data.title).toBe(testTitle);
